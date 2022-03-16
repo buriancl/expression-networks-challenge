e52caaa552f139eb1c3d80f9e06c13573dd22e35
buriancl <burian.christopher@gmail.com>
2022-03-16 10:37:54 -0400

    controllers setup

---
+++ server/controllers/building.js
@@ -0,0 +1,50 @@
+const Building = require("../models/building");
+
+exports.getAllBuildings = (req, res) => {
+  Building.find()
+    .then((building) => {
+      console.log({ building });
+      res.json(building);
+    })
+    .catch((err) =>
+      res.status(404).json({ message: "No building found", error: err.message })
+    );
+};
+
+exports.postCreateBuilding = (req, res) => {
+  Building.create(req.body)
+    .then((data) => {
+      console.log({ data });
+      res.json({ message: "Building successfully consructed", data });
+    })
+    .catch((err) =>
+      res.status(400).json({
+        message: "Unable to construct new building",
+        error: err.message,
+      })
+    );
+};
+
+exports.putUpdateBuilding = (req, res) => {
+  Building.findByIdAndUpdate(req.params.id, req.body)
+    .then((data) =>
+      res.json({ message: "Building successfully updated", data })
+    )
+    .catch((err) =>
+      res
+        .status(404)
+        .json({ message: "Failed to update building", error: err.message })
+    );
+};
+
+exports.deleteBuilding = (req, res) => {
+  Building.findByIdAndRemove(req.params.id, req.body).then((data) =>
+    res
+      .json({ message: "Building succesfully deleted", data })
+      .catch((err) =>
+        res
+          .status(404)
+          .json({ error: "Building not found", message: err.message })
+      )
+  );
+};
