d23ed4be6122a3c697b44f0b9033bd0800f1d837
buriancl <burian.christopher@gmail.com>
2022-03-16 13:54:42 -0400

    update building functionality added

--- client/src/components/BuildingItem.js
+++ client/src/components/BuildingItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UpdateBuilding from "./UpdateBuilding";
 
 const BuildingItem = ({ data }) => {
   const {
@@ -12,17 +13,20 @@ const BuildingItem = ({ data }) => {
   } = data;
   return (
     <li key={_id}>
-      <h2>{address}</h2>
-      <div className="building-description">
-        <h3>Description</h3>
-        <p>{description}</p>
+      <div>
+        <h2>{address}</h2>
+        <div className="building-description">
+          <h3>Description</h3>
+          <p>{description}</p>
+        </div>
+        <div className="building-data">
+          <p>Length of contract: {contractLength}</p>
+          <p>Maximum Occupancy: {maxOccupancy}</p>
+          <p>Zone Type: {zoneType}</p>
+        </div>
+        <p>Last update: {updatedAt}</p>
       </div>
-      <div className="building-data">
-        <p>Length of contract: {contractLength}</p>
-        <p>Maximum Occupancy: {maxOccupancy}</p>
-        <p>Zone Type: {zoneType}</p>
-      </div>
-      <p>Last update: {updatedAt}</p>
+      <UpdateBuilding _id={_id} />
     </li>
   );
 };
---
+++ client/src/components/UpdateBuilding.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const UpdateBuilding = ({ _id }) => {
+  const [data, setData] = useState({
+    address: "",
+    maxOccupancy: "",
+    description: "",
+    contractLength: "",
+    zoneType: "",
+  });
+
+  const handleChange = (e) => {
+    setData((data) => ({ ...data, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .put(`http://localhost:8000/api/building/${_id}`, data)
+      .then((res) => {
+        setData({
+          address: "",
+          maxOccupancy: "",
+          description: "",
+          contractLength: "",
+          zoneType: "",
+        });
+        console.log(res.data.message);
+      })
+      .catch((err) => {
+        console.log("Failed to update building", err.message);
+      });
+  };
+
+  return (
+    <form
+      className="form-container"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
+      <label htmlFor="address" className="label">
+        Address
+      </label>
+      <input
+        type="text"
+        name="address"
+        value={data.address}
+        onChange={handleChange}
+        className="input"
+      />
+      <label htmlFor="maxOccupancy" className="label">
+        Max Occupancy
+      </label>
+      <input
+        type="number"
+        name="maxOccupancy"
+        min="1"
+        value={data.maxOccupancy}
+        onChange={handleChange}
+        className="input"
+      />
+      <label htmlFor="description" className="label">
+        Description/Notes
+      </label>
+      <input
+        type="text"
+        name="description"
+        value={data.description}
+        onChange={handleChange}
+        className="input"
+      />
+      <label htmlFor="contractLength" className="label">
+        Contract Length
+      </label>
+      <input
+        type="number"
+        name="contractLength"
+        min="0"
+        value={data.contractLength}
+        onChange={handleChange}
+        className="input"
+      />
+      <label htmlFor="zoneType" className="label">
+        zoneType
+      </label>
+      <input
+        type="text"
+        name="zoneType"
+        value={data.zoneType}
+        onChange={handleChange}
+        className="input"
+      />
+      <button type="submit" className="button">
+        Create Building
+      </button>
+    </form>
+  );
+};
+
+export default UpdateBuilding;
