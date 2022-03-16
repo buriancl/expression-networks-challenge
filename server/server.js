7c57defad90c32e90d2eedf51a67e0155250e503
buriancl <burian.christopher@gmail.com>
2022-03-16 10:43:45 -0400

    connect server to routes

--- server/server.js
+++ server/server.js
@@ -5,10 +5,14 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const building = require("./routes/building");
+
 connectDB();
 
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
+
+app.use("api/building", building);
 
 const PORT = process.env.PORT || 8000;
 
