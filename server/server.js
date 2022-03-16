3262ec976968211e3a90edddc582e2440feeb343
buriancl <burian.christopher@gmail.com>
2022-03-16 10:17:25 -0400

    connecting MongoDB server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
 
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
